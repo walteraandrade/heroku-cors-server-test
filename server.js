32767bb48dec298047efb75ec3f70d17fc462780
Walter Andrade <walteraandrade@gmail.com>
2021-11-04 15:34:44 -0300

    added Server to console.log

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ cors_proxy.createServer({
     xfwd: false,
   },
 }).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+  console.log('Running CORS Anywhere Server on ' + host + ':' + port);
 });
